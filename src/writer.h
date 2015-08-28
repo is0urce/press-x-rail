7cfff44e69a06c5261150a50da777a655cb6389a
is0urce <azathoth@mail.ru>
2015-08-29 01:00:26 +0300

    writer.h (chunk writer)

--- src/writer.h
+++ src/writer.h
@@ -3,7 +3,12 @@
 // desc: class with implementation
 // auth: is0urce
 
-// 2-d point with real components
+// chunk format writer
+// chunk [name][data_size][data]
+// name is char8, data_size is chunk_size_t, data size of data_size
+// values written by write<_T>(key, value) in nodes, top node exposed by writer class
+// data can be nested, open(key) for create nesting
+// writing another value-size or opening another nested list at same level invalidates previous
 
 #pragma once
 
@@ -13,16 +18,18 @@
 #include <fstream>
 #include <string>
 #include <memory>
+#include <stdexcept>
 
 namespace px
 {
-	template <typename _I, typename _F>
 	class writer
 	{
+	public:
 		class node;
+
 	public:
 		typedef std::ofstream stream_t;
-		typedef uint64_t key_t;
+		typedef struct { char letter[8]; } key_t;
 		typedef uint64_t chunk_size;
 		typedef std::shared_ptr<node> node_ptr;
 
@@ -30,33 +37,73 @@ namespace px
 		class node
 		{
 		private:
-			stream_t *m_stream;
+			stream_t *m_stream_ptr;
 			node_ptr m_child;
-			unsigned int m_acc_size;
+			bool m_opened;
+			stream_t::streampos m_reserve;
+			chunk_size m_acc;
 
 		public:
-			node(stream_t *stream) { m_stream = stream; }
+			node(stream_t *stream_ptr) : m_acc(0), m_opened(true), m_stream_ptr(stream_ptr) {}
 			~node() {}
-
 		public:
-			template <typename _E>
-			void write(key_t key, _E element)
+			void close()
 			{
-				chunk_size element_size = sizeof(_E);
-				m_stream->write((char*)&key, sizeof(key));
-				m_stream->write((char*)&element_size, sizeof(element_size));
-				m_stream->write((char*)&element, element_size);
+				if (m_child)
+				{
+					m_child->m_opened = false;
+					m_child->close();
+
+					auto current = m_stream_ptr->tellp();
+					m_stream_ptr->seekp(m_reserve);
+					m_stream_ptr->write((char*)&m_child->m_acc, sizeof(m_child->m_acc));
+					m_stream_ptr->seekp(current);
+
+					m_child.reset();
+				}
+			}
+			node_ptr open(key_t key)
+			{
+				if (!m_opened) throw std::logic_error("writer::node::write() node closed");
+				close();
+
+				chunk_size reserved = 0;
+				m_stream_ptr->write((char*)&key, sizeof(key));
+				m_reserve = m_stream_ptr->tellp();
+				if (m_reserve == stream_t::streampos(-1)) throw std::runtime_error("stream buffer does not support tellp(), or it fails");
+				m_stream_ptr->write((char*)&reserved, sizeof(reserved));
+
+				m_child.reset(new node(m_stream_ptr));
+				return m_child;
 			}
 			void write(key_t key, const std::string &str)
 			{
+				if (!m_opened) throw std::logic_error("writer::node::write() node closed");
+				close();
+
 				chunk_size len = str.length();
-				m_stream->write((char*)&key, sizeof(key));
-				m_stream->write((char*)&len, sizeof(len));
-				m_stream->write(str.c_str(), len);
+				m_stream_ptr->write((char*)&key, sizeof(key));
+				m_stream_ptr->write((char*)&len, sizeof(len));
+				m_stream_ptr->write(str.c_str(), len);
+
+				m_acc += sizeof(key) + sizeof(len) + len;
 			}
 			void write(key_t key, const char *cstr)
 			{
 				write(key, std::string(cstr));
+			}
+			template <typename _E>
+			void write(key_t key, _E element)
+			{
+				if (!m_opened) throw std::logic_error("writer::node::write() node closed");
+				close();
+
+				chunk_size element_size = sizeof(_E);
+				m_stream_ptr->write((char*)&key, sizeof(key));
+				m_stream_ptr->write((char*)&element_size, sizeof(element_size));
+				m_stream_ptr->write((char*)&element, element_size);
+
+				m_acc += sizeof(key) + sizeof(element_size) + element_size;
 			}
 		};
 	private:
@@ -68,7 +115,7 @@ namespace px
 			init(path);
 		}
 		writer(stream_t stream) { init(); }
-		~writer() { m_stream.flush(); }
+		~writer() { flush(); }
 
 	private:
 		void init(const std::string &path)
@@ -87,5 +134,6 @@ namespace px
 
 	public:
 		node_ptr top() { return m_top; }
+		void flush() { m_top->close(); m_stream.flush(); }
 	};
 }
--- tests/writer_test.cpp
+++ tests/writer_test.cpp
@@ -10,7 +10,20 @@ using namespace px;
 
 TEST_CASE("writer", "[writer]")
 {
-	writer<int, float> w("test.sav");
-	w.top()->write('int_', 63);
-	w.top()->write('zomg', "yohohoyo");
+	writer w("test.sav");
+	w.top()->write({ "int_646" }, 0xf0);
+	w.top()->write({ "elevat7" }, 10000.0);
+	w.top()->write({ "zomg" }, "yohohoyo");
+	auto small = w.top()->open({ "slist" });
+	small->write({ "version" }, 1);
+	auto list = w.top()->open({ "list" });
+	list->write({ "item0" }, 1);
+	list->write({ "item1" }, 2);
+	w.top()->write({ "inter" }, 0xff);
+	REQUIRE_THROWS_AS(list->write({ "item3" }, 3), std::logic_error);
+	auto list2 = w.top()->open({ "list2" });
+	list2->write({ "mob1" }, 1);
+	list2->write({ "mob2" }, 3);
+	list2->write({ "mob3" }, 7);
+	list2->write({ "mob4" }, 15);
 }
